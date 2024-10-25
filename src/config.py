ef51de3ef27e5ccdccc67598b496add5240a72d0
Paulescu <plabartabajo@gmail.com>
2024-10-25 11:14:58 +0200

    Add try catch to logging middleware

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
-from pydantic_settings import BaseSettings
 from pydantic import Field
+from pydantic_settings import BaseSettings
+
 
 class ElasticsearchConfig(BaseSettings):
-    host: str = Field(default="http://localhost:9200", alias="ELASTICSEARCH_HOST")
-    index: str = Field(default="taxi_data_api", alias="ELASTICSEARCH_INDEX")
+    host: str = Field(default='http://localhost:9200', alias='ELASTICSEARCH_HOST')
+    index: str = Field(default='taxi_data_api', alias='ELASTICSEARCH_INDEX')
+
 
 elasticsearch_config = ElasticsearchConfig()
--- src/middleware.py
+++ src/middleware.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from elasticsearch import Elasticsearch
 from fastapi import Request
+from loguru import logger
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from src.config import elasticsearch_config
@@ -10,24 +11,28 @@ from src.config import elasticsearch_config
 # Initialize Elasticsearch client
 es = Elasticsearch([elasticsearch_config.host])
 
-class TimingMiddleware(BaseHTTPMiddleware):
 
+class TimingMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
-        if request.url.path == "/trips":
+        if request.url.path == '/trips':
             start_time = time.time()
             response = await call_next(request)
             process_time = time.time() - start_time
-            
+
             # Log to Elasticsearch
-            es.index(
-                index=elasticsearch_config.index,
-                body={
-                    "endpoint": "/trips",
-                    "method": request.method,
-                    "process_time": process_time,
-                    "timestamp": datetime.utcnow().isoformat()
-                }
-            )
-            
+            try:
+                es.index(
+                    index=elasticsearch_config.index,
+                    body={
+                        'endpoint': '/trips',
+                        'method': request.method,
+                        'process_time': process_time,
+                        'timestamp': datetime.utcnow().isoformat(),
+                    },
+                )
+            except Exception as e:
+                logger.error(f'Error logging to Elasticsearch: {e}')
+
             return response
+
         return await call_next(request)
